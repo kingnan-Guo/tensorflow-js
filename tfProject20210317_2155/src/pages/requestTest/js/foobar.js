c607a1045e79dec78ac72fd4e1631e29b972262e
kingnan <373734199@qq.com>
2021-12-19 02:31:49 +0800

    Revert "先提交代码"
    This reverts commit 10e08994db3a011ff449b788010cc44361ad56f4.

--- tfProject20210317_2155/src/pages/requestTest/js/foobar.js
+++
@@ -1,8 +0,0 @@
-function Foobar (params) {
-  this.foo = function () {
-    console.log('foo', params)
-  }
-}
-
-var foobar = new Foobar()
-exports.foobar = foobar
--- tfProject20210317_2155/src/pages/requestTest/js/requestA.js
+++
@@ -1,6 +0,0 @@
-
-var test = require('./foobar')
-console.log('test', test)
-console.log('test', test.foobar)
-test.foobar.constructor('22')
-test.foobar.foo()
